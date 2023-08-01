b6ca3395dfefa51c5accae428c983bfe1c426d5d
bramtechs <bramtech@telenet.be>
2023-08-01 22:04:58 +0200

    trying to add custom slideshow

--- php/comps/slideshow.php
+++ php/comps/slideshow.php
@@ -7,31 +7,23 @@ function slide(Game $slide)
 {
     $full_img = "/games/" . $slide->name . "/" . $slide->images[0];
     echo <<<HTML
-        <div class="carousel-item">
-            <img src="$full_img" alt="$slide->name">
+        <div class="slideshow-item">
+            <img draggable="false" src="$full_img" alt="$slide->name">
+            <div class="slideshow-description">
+                <h3>{$slide->title}</h3>
+                <p>{$slide->description}</p>
+                <a href="/games/{$slide->name}" class="btn btn-primary">Play</a>
+            </div>
+            
         </div>
 HTML;
 }
 
 ?>
 
-<div class="game-slideshow carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
-        <?php
-        $i = 0;
-        foreach ($games as $game) {
-            $class = "";
-            if ($i == 0) {
-                $class = "active";
-            }
-            echo <<<HTML
-            <li data-target="#carouselExampleIndicators" data-slide-to="{$i}" class="{$class}"></li>
-HTML;
-            $i++;
-        }
-        ?>
-    </ol>
-    <div class="carousel-inner">
+<link rel="stylesheet" href="/assets/css/slideshow.css">
+<div class="slideshow">
+    <div class="slideshow-inner">
         <?php
         foreach ($games as $game) {
             if ($game->visible && $game->featured) {
