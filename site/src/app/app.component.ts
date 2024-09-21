9397ac756d934e804171682c9a9586bddec56d22
bramtechs <bramtech@telenet.be>
2024-09-22 00:33:10 +0200

    start of footer and header component

--- site/src/app/app.component.ts
+++ site/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './comps/layout/header.component';
+import { FooterComponent } from './comps/layout/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'site';
---
+++ site/src/app/comps/layout/footer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-footer',
+  templateUrl: './footer.component.html',
+  standalone: true,
+})
+export class FooterComponent {}
